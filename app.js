6261886c7f9e1f87a6673ff6e210b7e19a28fc17
Gbell26 <107581827+Gbell26@users.noreply.github.com>
2022-11-29 13:31:23 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if(entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+const hiddenElements=document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
+
+const toggleNav = () => {
+    document.body.dataset.nav = document.body.dataset.nav === "true" ? "false" : "true";
+}
+
+window.addEventListener('click', function(e){
+    if (!document.getElementById('nav').contains(e.target) && (!document.getElementById('nav-toggle').contains(e.target))){
+    if(document.body.dataset.nav === "true") document.body.dataset.nav = "false";
+  } 
+})
